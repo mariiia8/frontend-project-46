d7a68a10e19e81f1537a7aeb94b408656fabd8e5
mariiia8 <mariasidorova567@gmail.com>
2025-12-01 19:54:42 +0300

    step2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,26 +1,30 @@
 #!/usr/bin/env node
 import genDiff from '../src/index.js';
-// Просто выводим точно то, что требуется
+
+import { Command } from 'commander';
+
+const program = new Command();
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0', '-V, --version', 'output the version number')
+  .option('-f, --format [type]', 'output format', 'stylish')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2, options) => {
+    console.log(`Comparing ${filepath1} and ${filepath2} with format: ${options.format}`);
+  });
+
+program.showHelpAfterError('(add --help for additional information)');
+
 if (process.argv.includes('-h') || process.argv.includes('--help')) {
-  console.log('\n  Usage: gendiff [options]\n');
+  console.log('\n  Usage: gendiff [options] <filepath1> <filepath2>\n');
   console.log('  Compares two configuration files and shows a difference.\n');
   console.log('  Options:');
   console.log('    -V, --version        output the version number');
+  console.log('    -f, --format [type]  output format');
   console.log('    -h, --help           display help for command');
   process.exit(0);
 }
 
-if (process.argv.includes('-V') || process.argv.includes('--version')) {
-  console.log('1.0.0');
-  process.exit(0);
-}
-
-// Если переданы аргументы файлов
-if (process.argv.length >= 4) {
-  const filepath1 = process.argv[2];
-  const filepath2 = process.argv[3];
-  console.log(`Comparing ${filepath1} and ${filepath2}`);
-} else {
-  console.log('Please provide two file paths');
-  console.log('Example: gendiff file1.json file2.json');
-}
+program.parse(process.argv);
--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-export default (filepath1, filepath2) => {
-  return `Comparing ${filepath1} and ${filepath2}`;
+export default (filepath1, filepath2, format = 'stylish') => {
+  return `Comparing ${filepath1} and ${filepath2} with format: ${format}`;
 };
